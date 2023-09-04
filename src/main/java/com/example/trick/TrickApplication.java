c9ececbae057e8ad22eef4d0dd556ebe5d83ace8
N.Demin <N.Demin@ase-ec.ru>
2023-09-04 17:11:22 +0300

    First commit

---
+++ src/main/java/com/example/trick/TrickApplication.java
@@ -0,0 +1,13 @@
+package com.example.trick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trick/TrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
